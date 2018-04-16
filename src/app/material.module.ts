8a9e594d0a0d4f437b3bb785b480b3d27733851c
Irakli <i.dzigrashvili@gmail.com>
2018-04-16 23:10:15 +0400

    second commit

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,12 +1,15 @@
-import { NgModule } from "@angular/core";
-import {MatButtonModule} from '@angular/material/button';
-
+import { NgModule } from '@angular/core';
+import {
+  MatButtonModule,
+  MatIconModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatDatepickerModule,
+  MatNativeDateModule
+} from '@angular/material';
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule]
+  imports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule,MatDatepickerModule, MatNativeDateModule],
+  exports: [MatButtonModule, MatIconModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule]
 })
-
-export class MaterialModule {
-   
-}
+export class MaterialModule {}
